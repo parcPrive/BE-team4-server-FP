7ffe8273fc73dff413ea29566bf6fb583aed9169
mok <99533961+parcPrive@users.noreply.github.com>
2024-01-23 14:51:50 +0900

    상품태그_수정중

--- src/main/java/com/kj/notice/NoticeController.java
+++ src/main/java/com/kj/notice/NoticeController.java
@@ -1,14 +1,21 @@
 package com.kj.notice;
 
+import com.kj.faq.dto.FaqBoardDto;
+import com.kj.faq.dto.FaqCategoryDto;
 import com.kj.faq.entity.FaqBoard;
+import com.kj.faq.entity.FaqCategory;
 import com.kj.member.dto.CustomUserDetails;
 import com.kj.notice.dto.NoticeDto;
 import com.kj.notice.entity.Notice;
+import com.kj.noticeComment.CommentService;
+import com.kj.noticeComment.entity.Comment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +27,7 @@ import java.util.List;
 @Slf4j
 public class NoticeController {
     private final NoticeService noticeService;
+    private final CommentService commentService;
     private int paginationSize=5;
     @GetMapping("/list")
     public String noticeList(Model model, @RequestParam(value = "page", required = true, defaultValue = "0") int page){
@@ -50,13 +58,37 @@ public class NoticeController {
     @GetMapping("/view/{id}/{no}")
     public String viewNotice(@PathVariable Long id,
                              @PathVariable int no,Model model,@RequestParam(value = "page", required = true, defaultValue = "0") int page){
-        List<Notice> noticeList = noticeService.findByBetweenNum(no);
-        Page<Notice> pagination = noticeService.noticeList(page);
-        Notice noticeInfo = noticeService.findById(id);
+        Page<Notice> pagination = noticeService.viewNoticeList(page,no);
+        Notice noticeInfo = noticeService.findByIdPlusView(id);
+        List<Comment> commentList = commentService.commentList(id);
+        List<Notice> noticeList = pagination.getContent();
         model.addAttribute("noticeInfo",noticeInfo);
         model.addAttribute("pagination",pagination);
         model.addAttribute("noticeList",noticeList);
+        model.addAttribute("commentList",commentList);
         model.addAttribute("no",no);
         return "/notice/viewNotice";
+    }
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable Long id, Model model) {
+        Notice noticeInfo = noticeService.findById(id);
+        model.addAttribute("noticeInfo",noticeInfo);
+        return "/notice/updateNotice";
+    }
+    @Transactional
+    @PostMapping("/update")
+    public String updateProcess(@RequestParam Long id, @ModelAttribute NoticeDto noticeDto,
+                                @AuthenticationPrincipal CustomUserDetails customUserDetails){
+        noticeService.updateNotice(noticeDto);
+        return "redirect:/cs/notice/list";
+    }
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        boolean isDelete = noticeService.deleteById(id);
+        if(isDelete){
+            return "redirect:/cs/notice/list";
+        }else {
+            return "redirect:/";
+        }
     }
 }
